c55ff4bcf27cc81964c50d221fa3cfdf9d24ced1
postromantic <postromantic@gmail.com>
2024-04-15 14:11:22 +0200

    increase max line len

--- index/write.go
+++ index/write.go
@@ -92,7 +92,7 @@ func makePostEntry(trigram, fileid uint32) postEntry {
 // or if it contains more than maxTextTrigrams distinct trigrams.
 const (
 	maxFileLen      = 1 << 30
-	maxLineLen      = 3000
+	maxLineLen      = 4096
 	maxTextTrigrams = 20000
 )
 
